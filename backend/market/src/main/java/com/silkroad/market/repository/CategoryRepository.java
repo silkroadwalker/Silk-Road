67d26abfa2fc1073f3824fb1e449ad271bbfcfae
silkroadwalker <sajjadheidari289@gmail.com>
2026-07-01 12:08:56 +0330

    create category entity

---
+++ backend/market/src/main/java/com/silkroad/market/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.silkroad.market.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.silkroad.market.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+
+    boolean existsByName(String name);
+}
