34c46215b09700965a9991caebdb876661fdc315
ferimuferferi <ferimuferferi1205@gmail.com>
2026-07-06 23:00:24 +0330

    feat: added create ad fxml

---
+++ frontend/src/main/java/com/silkroad/ui/CreateAdController.java
@@ -0,0 +1,32 @@
+package com.silkroad.ui;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class CreateAdController {
+    @FXML
+    private Label headerLabel;
+    @FXML
+    private TextField titleField;
+    @FXML
+    private TextArea descriptionField;
+    @FXML
+    private TextField priceField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private TextField categoryField;
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private void handleSubmit() {
+
+    }
+
+    @FXML
+    private void goBack() {
+
+    }
+}
