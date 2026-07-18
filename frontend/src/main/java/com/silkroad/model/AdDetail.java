bed76c0f6aa7ae5de8f76898d7422e104e361f92
ferimuferferi <ferimuferferi1205@gmail.com>
2026-07-18 12:54:03 +0330

    feat: added AdDetail class

--- frontend/src/main/java/com/silkroad/model/AdDetail.java
+++ frontend/src/main/java/com/silkroad/model/AdDetail.java
@@ -1,4 +1,21 @@
 package com.silkroad.model;
 
-public class AdDetail {
+public class AdDetail extends Ad {
+    private String sellerFullName;
+    private String sellerPhone;
+    private String rejectionReason;
+    private boolean submitter; // true if the logged-in user owns this ad
+    private Double averageRating;
+
+    public String getSellerFullName() { return sellerFullName; }
+    public String getSellerPhone() { return sellerPhone; }
+    public String getRejectionReason() { return rejectionReason; }
+    public boolean isSubmitter() { return submitter; }
+    public Double getAverageRating() { return averageRating; }
+
+    public void setSellerFullName(String sellerFullName) { this.sellerFullName = sellerFullName; }
+    public void setSellerPhone(String sellerPhone) { this.sellerPhone = sellerPhone; }
+    public void setRejectionReason(String rejectionReason) { this.rejectionReason = rejectionReason; }
+    public void setSubmitter(boolean submitter) { this.submitter = submitter; }
+    public void setAverageRating(Double averageRating) { this.averageRating = averageRating; }
 }
